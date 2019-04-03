ba58585a03b3ae1c04ff0b796fdd39f1250cb717
Jenny Heath <jennifer.w.heath@gmail.com>
2019-04-02 20:16:21 -0700

    copy over webpack and babel dependencies from selenes website

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+  entry: "./src/index.js",
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        options: { presets: ['@babel/preset-env'] }
+      },
+      {
+        test: /\.css$/,
+        use: [ 'style-loader', 'css-loader' ]
+      },
+      {
+        test: /\.(png|jpg|gif|mp3)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {}
+          }
+        ]
+      }
+    ]
+  },
+  resolve: { extensions: ['*', '.js', '.jsx'] },
+  output: {
+    path: path.resolve(__dirname, "dist/"),
+    publicPath: "/dist/",
+    filename: "bundle.js"
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "/"),
+    port: 3000,
+    publicPath: "http://localhost:3000/dist/",
+    historyApiFallback: true,
+    hotOnly: true
+  },
+  plugins: [ new webpack.HotModuleReplacementPlugin() ]
+};
